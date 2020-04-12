01ac2619935d52b14db3329251d66a991e0aaacc
tushar <tushar.mittal@mountblue.tech>
2020-04-12 14:20:12 +0530

    new ec2 settings

--- .flaskenv
+++ .flaskenv
@@ -3,6 +3,6 @@ FLASK_APP = app.py
 
 FLASK_ENV=development
 
-ELASTICSEARCH_URL = vpc-flask-es-zbr5naqwca7hbqcjypjsbkvir4.us-east-2.es.amazonaws.com:80
+ELASTICSEARCH_URL = vpc-corona-flask-es-jd6nq2w2lvkt4h7yloixdeh2au.us-east-2.es.amazonaws.com:80
 
 FLASK_DEBUG=1
--- config.py
+++ config.py
@@ -8,9 +8,9 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config(object):
     SEARCH_INDEX = 'data_covid'
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    SQLALCHEMY_DATABASE_URI = "postgres://postgres:db-flask123@flask-db.ckgbqi9rzpou.us-east-2.rds.amazonaws.com:5432/postgres"    
+    SQLALCHEMY_DATABASE_URI = "postgres://postgres:tushar-123@corona-flask-db.ckgbqi9rzpou.us-east-2.rds.amazonaws.com:5432/postgres"    
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    ELASTICSEARCH_URL = "vpc-flask-es-zbr5naqwca7hbqcjypjsbkvir4.us-east-2.es.amazonaws.com:80"
+    ELASTICSEARCH_URL = "vpc-corona-flask-es-jd6nq2w2lvkt4h7yloixdeh2au.us-east-2.es.amazonaws.com:80"
     
     SEARCH_INDEX = 'forum_data'
 
